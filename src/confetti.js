ac5c4a319f1a845148e8f0256e84a1a61e37d4a8
Daniel Lundin <d-dog@d-dog.se>
2017-04-11 22:04:10 +0200

    Initial commit

---
+++ src/confetti.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { confetti } from 'dom-confetti';
+
+const style = {
+  position: 'relative'
+};
+
+export default class Confetti extends Component {
+  constructor(props) {
+    super(props);
+    this.setRef = this.setRef.bind(this);
+  }
+
+  componentWillRecieveProps(nextProps) {
+    if (nextProps.show && !this.props.show) {
+      confetti(this.container, nextProps.config);
+    }
+  }
+
+  setRef(ref) {
+    this.container = ref;
+  }
+
+  render() {
+    return <div style={ style } ref={ this.setRef } />;
+  }
+}
